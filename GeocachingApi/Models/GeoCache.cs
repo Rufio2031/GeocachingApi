468fac42e0d0980af3f001088a2c59012029bed2
Rufio2031 <BBieniek14@gmail.com>
2020-02-06 21:55:11 -0600

    Initial project commit

---
+++ GeocachingApi/Models/GeoCache.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeocachingApi.Models
+{
+    public class GeoCache
+    {
+    }
+}
