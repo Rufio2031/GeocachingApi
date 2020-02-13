2346ce3742651d6123f57a25649f67549a30a698
Bryan Bieniek <Bryan.Bieniek@pattersoncompanies.com>
2020-02-13 14:41:47 -0600

    Add comment to ToSafeList extension

--- GeocachingApi.Infrastructure/Extensions/EnumerableExtensions.cs
+++ GeocachingApi.Infrastructure/Extensions/EnumerableExtensions.cs
@@ -4,6 +4,12 @@ namespace GeocachingApi.Infrastructure.Extensions
 {
     public static class EnumerableExtensions
     {
+        /// <summary>
+        /// Converts an IEnumerable to a List of type T. If the collection is null just returns a new empty List of type T.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="enumerable">The enumerable.</param>
+        /// <returns></returns>
         public static IList<T> ToSafeList<T>(this IEnumerable<T> enumerable)
         {
             return ReferenceEquals(null, enumerable) ? new List<T>() : (enumerable is IList<T>) ? (IList<T>)enumerable : new List<T>(enumerable);
